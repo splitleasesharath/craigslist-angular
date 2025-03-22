7c10b8ebf8d8793ac2b7de6fc348dd840b7e1151
Ahsan Ahmad <splitleaseahsan@gmail.com>
2025-03-22 03:19:05 -0500

    Add Split lease

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MPOSupplier
+# SplitLease
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.6.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mposupplier'),
+      dir: require('path').join(__dirname, './coverage/splitlease'),
       subdir: '.',
       reporters: [
         { type: 'html' },
